3263d1b2fb6200f23d4623949cdc5c4082368090
Mardan21 <47092419+Mardan21@users.noreply.github.com>
2025-07-15 13:12:39 -0400

    Create consecutive_numbers.sql

---
+++ consecutive_numbers.sql
@@ -0,0 +1,12 @@
+WITH cte AS (
+    SELECT
+        num,
+        LEAD(num, 1) OVER() num1,
+        LEAD(num, 2) OVER() num2
+    FROM 
+        Logs
+)
+
+SELECT DISTINCT num ConsecutiveNums
+FROM cte
+WHERE (num = num1) AND (num = num2)
