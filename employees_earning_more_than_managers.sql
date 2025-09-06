011022bdd72d1700fb14f5fde3670f9602aeffbe
Mardan21 <47092419+Mardan21@users.noreply.github.com>
2025-09-05 22:57:13 -0400

    Update employees_earning_more_than_managers.sql

--- employees_earning_more_than_managers.sql
+++ employees_earning_more_than_managers.sql
@@ -1,1 +1,14 @@
+WITH EmployeeManagerPairings AS (   
+    SELECT 
+        e.id AS employeeId, 
+        e.name, 
+        e.salary AS employeeSalary, 
+        m.id AS managerId, 
+        m.salary AS managerSalary
+    FROM Employee e
+    JOIN Employee m ON e.managerId = m.id
+)
 
+SELECT name AS Employee
+FROM EmployeeManagerPairings
+WHERE employeeSalary > managerSalary;
